67b4679c9a75af1e6e82c21d08db8501d1c9b804
LazyGalaxy <vangos@lazygalaxy.com>
2023-10-19 22:51:00 +0200

    scrolling introduced

--- instalgosimple/bot.py
+++ instalgosimple/bot.py
@@ -1,13 +1,17 @@
 import cv2 as cv
+import pyautogui
 from time import sleep
 from threading import Thread, Lock
 from vision import Vision
+from windowcapture import WindowCapture
 
 
 class BotState:
-    INITIALIZING = 0
-    SEARCHING = 1
-    CLICKING = 2
+    SCREEN_CAPTURING = 0
+    IDLING = 1
+    UP_SCROLLING = 2
+    DOWN_SCROLLING = 3
+    LIKE_CLICKING = 4
 
 
 class LikeBot:
@@ -16,30 +20,43 @@ class LikeBot:
     lock = None
 
     # properties
-    state = None
-    screenshot = None
-    window_offset = (0, 0)
-    window_w = 0
-    window_h = 0
+    frequency = None
     debug = None
+    wincap = None
+    screenshot = None
+    window_offset = None
+    state = None
 
     # vision
     vision_like_icon = None
     vision_more_icon = None
 
-    def __init__(self, window_offset, window_size, debug=False):
+    def __init__(self, frequecy=0.5, debug=False):
         # create a thread lock object
         self.lock = Lock()
+
+        self.debug = debug
+        self.frequency = frequecy
+
+        # initialize the WindowCapture class
+        self.wincap = WindowCapture("BlueStacks App Player")
+
+        self.screenshot = self.wincap.get_screenshot()
+        while self.screenshot is None:
+            # wait until we get the first screenshot
+            print("no screenshot, waiting for 1 sec...")
+            sleep(1)
+            self.screenshot = self.wincap.get_screenshot()
+
+        if debug:
+            cv.imshow("Matches", self.screenshot)
 
         # for translating window positions into screen positions, it's easier to just
         # get the offsets and window size from WindowCapture rather than passing in
         # the whole object
-        self.window_offset = window_offset
-        self.window_w = window_size[0]
-        self.window_h = window_size[1]
+        self.window_offset = (self.wincap.offset_x, self.wincap.offset_y)
 
-        self.state = BotState.INITIALIZING
-        self.debug = debug
+        self.state = BotState.SCREEN_CAPTURING
 
         # initialize the Vision class
         self.vision_like_icon = Vision(
@@ -50,34 +67,35 @@ class LikeBot:
         )
 
     def click_like_targets(self, like_targets):
+        self.state = BotState.IDLING
         for like_target in like_targets:
             if self.stopped:
                 break
 
-            screen_x, screen_y = self.get_screen_position(like_target)
+            self.state = BotState.LIKE_CLICKING
+            screen_x, screen_y = self.get_click_position(like_target)
             # move the mouse
             # pyautogui.moveTo(x=screen_x, y=screen_y)
             # pyautogui.click()
-            print("clicked on x:{} y:{}".format(screen_x, screen_y))
+            # print("clicked on x:{} y:{}".format(screen_x, screen_y))
+
+    def down_scroll(self, clicks):
+        self.state = BotState.DOWN_SCROLLING
+        x, y = self.get_click_position(self.get_screen_midpoint())
+        pyautogui.scroll(clicks, x, y)
 
     # translate a pixel position on a screenshot image to a pixel position on the screen.
     # pos = (x, y)
     # WARNING: if you move the window being captured after execution is started, this will
     # return incorrect coordinates, because the window position is only calculated in
     # the WindowCapture __init__ constructor.
-    def get_screen_position(self, pos):
+    def get_click_position(self, pos):
         return (pos[0] + self.window_offset[0], pos[1] + self.window_offset[1])
 
+    def get_screen_midpoint(self):
+        return (int(self.wincap.w / 2), int(self.wincap.h / 2))
+
     # threading methods
-
-    def update_screenshot(self, screenshot):
-        self.lock.acquire()
-        self.screenshot = screenshot
-        if screenshot is None:
-            self.like_targets = None
-            self.more_targets = None
-        self.lock.release()
-
     def start(self):
         self.stopped = False
         t = Thread(target=self.run)
@@ -89,45 +107,43 @@ class LikeBot:
     # main logic controller
     def run(self):
         while not self.stopped:
-            sleep(1)
+            sleep(self.frequency)
             self.lock.acquire()
 
-            if self.debug:
-                if self.screenshot is None:
-                    like_targets = []
-                    more_targets = []
-                else:
-                    like_targets = self.vision_like_icon.find(
-                        self.screenshot, 0.8, (0, 0, 255)
-                    )
-                    more_targets = self.vision_more_icon.find(
-                        self.screenshot, 0.8, (255, 0, 255)
-                    )
-                    if self.debug:
-                        cv.imshow("Matches", self.screenshot)
+            self.screenshot = self.wincap.get_screenshot()
 
-                print(
-                    str(self.state)
-                    + " like:"
-                    + str(like_targets)
-                    + " more:"
-                    + str(more_targets)
+            if self.screenshot is None:
+                like_targets = []
+                more_targets = []
+                self.state = BotState.SCREEN_CAPTURING
+            else:
+                like_targets = self.vision_like_icon.find(
+                    self.screenshot, 0.8, (0, 0, 255)
                 )
+                more_targets = self.vision_more_icon.find(
+                    self.screenshot, 0.8, (255, 0, 255)
+                )
+                if self.debug:
+                    print(str(self.wincap.w) + " " + str(self.wincap.h))
+                    cv.drawMarker(
+                        self.screenshot,
+                        self.get_screen_midpoint(),
+                        (0, 255, 255),
+                        markerType=cv.MARKER_CROSS,
+                        markerSize=300,
+                        thickness=2,
+                    )
+                    cv.imshow("Matches", self.screenshot)
 
-            if self.state == BotState.INITIALIZING:
-                if like_targets:
-                    self.state = BotState.SEARCHING
+            self.click_like_targets(like_targets)
+            self.down_scroll(-3)
 
-            elif self.state == BotState.SEARCHING:
-                if like_targets:
-                    self.click_like_targets(like_targets)
-                    self.state = BotState.CLICKING
-
-            elif self.state == BotState.CLICKING:
-                if like_targets:
-                    self.click_like_targets(like_targets)
-                    self.state = BotState.CLICKING
-                else:
-                    self.state = BotState.SEARCHING
+            print(
+                str(self.state)
+                + " like:"
+                + str(like_targets)
+                + " more:"
+                + str(more_targets)
+            )
 
             self.lock.release()
--- instalgosimple/main.py
+++ instalgosimple/main.py
@@ -1,40 +1,16 @@
 import cv2 as cv
-import numpy as np
 import os
-from time import sleep
-from windowcapture import WindowCapture
 from bot import LikeBot
 
 # Change the working directory to the folder this script is in.
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
-
-# initialize the WindowCapture class
-wincap = WindowCapture("BlueStacks App Player")
-
-screenshot = wincap.get_screenshot()
-while screenshot is None:
-    # wait until we get a screenshot
-    print("no screenshot, waiting for 1 sec...")
-    sleep(1)
-    screenshot = wincap.get_screenshot()
-if DEBUG:
-    cv.imshow("Matches", screenshot)
-
 # initialize the like bot
-bot = LikeBot((wincap.offset_x, wincap.offset_y), (wincap.w, wincap.h), DEBUG)
+bot = LikeBot(frequecy=2, debug=True)
 bot.start()
 
-
 while True:
-    # if we don't have a screenshot yet, don't run the code below this point yet
-    if screenshot is None:
-        bot.update_screenshot(None)
-    else:
-        bot.update_screenshot(screenshot)
-
     # press 'q' with the output window focused to exit.
     # waits 1 ms every loop to process key presses
     key = cv.waitKey(1)
@@ -42,8 +18,5 @@ while True:
         bot.stop()
         cv.destroyAllWindows()
         break
-
-    # get an updated image of the game
-    screenshot = wincap.get_screenshot()
 
 print("Done.")
